978b1c279d2983a4130e14bb8772782d8e537378
Andrei Motinga <andrew.motinga@gmail.com>
2018-07-27 14:31:17 -0400

    Add styling

--- app/javascript/quotes/components/QuotesDisplay.jsx
+++ app/javascript/quotes/components/QuotesDisplay.jsx
@@ -1,67 +1,78 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-// parsing params string
-import queryString from 'query-string'
-import axios from 'axios'
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+import queryString from 'query-string';
+import axios from 'axios';
+import QuoteText from './QuoteText';
+import QuoteNavigation from './QuoteNavigation';
+import QuoteFooter from './QuoteFooter';
 
 class QuotesDisplay extends React.Component {
-  state = {
-    quote: []
-  }
-
-  componentDidMount () {
-    this.init(this.props)
-  }
-
-  componentWillReceiveProps (nextProps) {
-    this.init(nextProps)
-  }
-
-  init (props) {
-    const params = props.location.search
-    const quoteId = this.setQuoteIdFromQueryString(params)
-    this.fetchQuote(quoteId);
-  }
-
-  fetchQuote (id) {
-    axios.get(`/api/quotes/${id}`)
-      .then(res => {
-        this.setState({ quote: res.data })
-      })
-      .catch(err => {
-        console.error(err)
-      })
-  }
-
-  setQuoteIdFromQueryString (params) {
-    const qsParams = queryString.parse(params)
-    const quote = qsParams.quote
-    if (quote) {
-      return Number(quote)
-    } else {
-      const startingQuoteId = this.props.startingQuoteId
-      this.setInitialQuoteId(startingQuoteId)
-      return startingQuoteId
+  constructor () {
+    super()
+    this.state = {
+      quote: {},
+      fireRedirect: false
     }
   }
 
-  setInitialQuoteId (startingQuoteId) {
-    this.props.history.push(`/?quote=${startingQuoteId}`)
+  fetchQuote (id) {
+    axios.get(`api/quotes/${id}`)
+      .then(response => {
+        this.setState({ quote: response.data })
+      })
+      .catch(error => {
+        console.error(error)
+        this.setState({ fireRedirect: true })
+      })
+  }
+
+  setQuoteIdFromQueryString (qs) {
+    this.qsParams = queryString.parse(qs)
+    if (this.qsParams.quote) {
+      // assign quote ID from the URL's query string
+      this.quoteId = Number(this.qsParams.quote)
+    } else {
+      this.quoteId = this.props.startingQuoteId
+      // update URL in browser to reflect current quote in query string
+      this.props.history.push(`/?quote=${this.quoteId}`)
+    }
+  }
+
+  componentDidMount () {
+    this.setQuoteIdFromQueryString(this.props.location.search)
+    this.fetchQuote(this.quoteId)
+  }
+
+  componentWillReceiveProps (nextProps) {
+    this.setQuoteIdFromQueryString(nextProps.location.search)
+    this.fetchQuote(this.quoteId)
   }
 
   render () {
-    const { quote } = this.state
-    const { text, author, next_id, previous_id } = quote
+    const quote = this.state.quote
+    const nextQuoteId = quote.next_id
+    const previousQuoteId = quote.previous_id
 
     return (
       <div>
-        {previous_id && <Link to={`/?quote=${previous_id}`}>Previous</Link>}
-        <p>{text}</p>
-        <p>{author}</p>
-        {next_id && <Link to={`/?quote=${next_id}`}>Next</Link>}
+        <div className='quote-container'>
+          {this.state.fireRedirect &&
+          <Redirect to={'/'} />
+          }
+          {previousQuoteId &&
+          <QuoteNavigation direction='previous' otherQuoteId={previousQuoteId} />
+          }
+          <QuoteText quote={this.state.quote} />
+          {nextQuoteId &&
+          <QuoteNavigation direction='next' otherQuoteId={nextQuoteId} />
+          }
+        </div>
+        {this.state.quote.id !== parseInt(this.props.startingQuoteId, 10) &&
+        <QuoteFooter startingQuoteId={this.props.startingQuoteId} />
+        }
       </div>
-    )
-  }
+      )
+}
 }
 
-export default QuotesDisplay;
+export default QuotesDisplay
